c5ee2d1f9eb5d13d8cde8ee77fcfb23a27ab5fab
JARAK22 <jarak200122@gmail.com>
2025-05-09 07:56:10 -0500

    cambio

--- js/logica.js
+++ js/logica.js
@@ -1,3 +1,4 @@
+// primer ejercicio
 function calPrecioTotal() {
   let precio = parseInt(document.getElementById("precio").value)
   let iva = parseInt(document.getElementById("iva").value)
@@ -11,6 +12,7 @@ function calPrecioTotal() {
   }
 }
 
+// Segundo ejercicio
 function perimetoCuadrado() {
   let ladoCuadrado = parseInt(document.getElementById("LCuadrado").value)
   if (ladoCuadrado) {
@@ -25,6 +27,7 @@ function perimetoCuadrado() {
   }
 }
 
+// Tercer ejercicico
 function nombreUsuario() {
   let nombre = document.getElementById("usurio").value
   if (nombre) {
@@ -34,6 +37,7 @@ function nombreUsuario() {
   }
 }
 
+// Cuarto ejercicio
 function valorMedio() {
   let num1 = parseInt(document.getElementById("numero1").value)
   let num2 = parseInt(document.getElementById("numero2").value)
@@ -46,7 +50,7 @@ function valorMedio() {
       document.getElementById("numeroMedio").innerHTML =
         "El valor del medio es: " + num2
     }else {
-    	document.getElementById("numeroMedio").innerHTML =
+      document.getElementById("numeroMedio").innerHTML =
       "El valor del medio es: " + num3
     }*/
     let media = (num1 + num2 + num3) / 3;
@@ -54,5 +58,90 @@ function valorMedio() {
         "La media de los tres numeros es: " + media
   } else {
     alert("Por favor llena ambos campos.")
+  }
+}
+
+// Quinto ejercicicio
+function consumoCombustible() {
+  let kilometros = parseInt(document.getElementById("km").value)
+  let gasolina = parseInt(document.getElementById("litros").value)
+  if (kilometros && gasolina) {
+    let totalCosumo = gasolina / kilometros;
+    document.getElementById("consumido").innerHTML = "El consumo de combustible por kilómetros es de: " + totalCosumo
+  } else {
+    alert("Por favor llena ambos campos.")
+  }
+}
+
+// Sexto ejercicio
+function convertirHM() {
+  let horas = parseInt(document.getElementById("seconsH").value)
+  let minutos = parseInt(document.getElementById("seconsM").value)
+  if (horas && minutos) {
+    let segundosHoras = horas * 60 * 60;
+    let segundosMinutos = minutos * 60;
+    document.getElementById("sHoras").innerHTML = "Horas a segundos: " + segundosHoras + " Segundos";
+    document.getElementById("sMinutos").innerHTML = "Minutos a segundos: " + segundosMinutos + " Segundos";
+  } else {
+    alert("Por favor llena ambos campos.")
+  }
+}
+
+// Septimo ejercicio
+function decenasUnidades() {
+  let numero = parseInt(document.getElementById("numero").value)
+  if (numero) {
+    let decenas = Math.floor(numero / 10);
+    let unidades = numero % 10;
+    document.getElementById("decenas").innerHTML = `${decenas} decenas`
+    document.getElementById("unidades").innerHTML = `${unidades} decenas`
+  } else {
+    alert("Por favor llena ambos campos.")
+  }
+}
+
+// Octavo ejercicio
+function cantidadIgredientes() {
+  let personas = parseInt(document.getElementById("personas").value)
+  if (personas) {
+    let papaGramos = personas * 200;
+    let cebollasGramos = (300 / 1000) * papaGramos;
+    let huevos = papaGramos / 1000 * 5;
+    document.getElementById("tortaPapa").innerHTML = `Se necesitan ${papaGramos} gramos de papa, ${huevos} huevos y ${cebollasGramos} gramos de cebolla`;
+  } else {
+    alert("Por favor llena ambos campos.")
+  }
+}
+
+// Noveno ejercicio
+function cambioVariable() {
+  let input1 = document.getElementById("valor1");
+  let input2 = document.getElementById("valor2");
+
+  // Valores como números
+  let valor1 = parseInt(input1.value);
+  let valor2 = parseInt(input2.value);
+
+  if (!isNaN(valor1) && !isNaN(valor2)) {
+    // Intercambio de valores
+    let temporal = valor1;
+    input1.value = valor2;
+    input2.value = temporal;
+  } else {
+    alert("Por favor llena ambos campos con números.");
+  }
+}
+
+// Decimo ejercicio
+function parImpar() {
+  let numParImpar = parseInt(document.getElementById("parImpar").value);
+
+  // Valores como números
+  if (!isNaN(numParImpar)) {
+    // Intercambio de valores
+    let resultado = numParImpar % 2 === 0;
+    document.getElementById("resultadoParImpar").innerHTML = `${numParImpar} es ${resultado}`
+  } else {
+    alert("Por favor llena ambos campos con números.");
   }
 }
